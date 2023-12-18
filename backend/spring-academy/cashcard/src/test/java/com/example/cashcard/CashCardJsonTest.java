23476e8b696684ea14b7c82086335b6ed7d35b0c
GeorgeFreelanceDeveloper <jiri.tester@outlook.com>
2023-12-18 20:31:15 +0100

    CashCardApplication: create CashCard json serialize and deserialize test

--- backend/spring-academy/cashcard/src/test/java/com/example/cashcard/CashCardJsonTest.java
+++ backend/spring-academy/cashcard/src/test/java/com/example/cashcard/CashCardJsonTest.java
@@ -1,12 +1,42 @@
 package com.example.cashcard;
 
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+
+import java.io.IOException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+@JsonTest
 public class CashCardJsonTest {
+    @Autowired
+    private JacksonTester<CashCard> json;
 
     @Test
-    void myFirstTest(){
-        assertThat(42).isEqualTo(42);
+    void cashCardSerializationTest() throws IOException {
+        final CashCard cashCard = new CashCard(99L, 123.45);
+        final JsonContent<CashCard> jsonContent = json.write(cashCard);
+        assertThat(jsonContent).isStrictlyEqualToJson("expected.json");
+        assertThat(jsonContent).hasJsonPathNumberValue("@.id");
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.id").isEqualTo(99);
+        assertThat(jsonContent).hasJsonPathNumberValue("@.amount");
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
+    }
+
+    @Test
+    void cashCardDeserializationTest() throws IOException {
+        final String expected = """
+                           {
+                               "id":99,
+                               "amount":123.45
+                           }
+                """;
+        assertThat(json.parse(expected)).isEqualTo(new CashCard(99L, 123.45));
+        assertThat(json.parseObject(expected).id()).isEqualTo(99);
+        assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
     }
 }
