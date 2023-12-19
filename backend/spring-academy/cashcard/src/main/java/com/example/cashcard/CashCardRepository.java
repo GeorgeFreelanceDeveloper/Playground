9476c70d9a360d8b1a3bf3397e9eb55758f986b5
GeorgeFreelanceDeveloper <jiri.tester@outlook.com>
2023-12-19 13:50:06 +0100

    CashCardApplication: fixed Not a managed type: class com.example.cashcard.CashCard

--- backend/spring-academy/cashcard/src/main/java/com/example/cashcard/CashCardRepository.java
+++ backend/spring-academy/cashcard/src/main/java/com/example/cashcard/CashCardRepository.java
@@ -1,8 +1,6 @@
 package com.example.cashcard;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-interface CashCardRepository extends CrudRepository<CashCard, Long>{
+interface CashCardRepository extends CrudRepository<CashCard, Long> {
 }
